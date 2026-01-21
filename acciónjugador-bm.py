df036909e9180872ee5781383d079fbf22974894
PabloAsisMuñoz <pabloasis02@gmail.com>
2026-01-21 14:28:19 +0100

    Changes

---
+++ acciónjugador-bm.py
@@ -0,0 +1,16 @@
+#En esta parte solicitaremos la acción del jugador
+opcion = input("Ingrese una de las tres opciones posible D para destapar, M para marcar U para desmarcar    : ")
+
+#Procesamos la acción del jugador
+if opcion == "D":
+    #Solicitamos la fila y la columna a destapar
+    fila = int(input("Ingrese la fila : "))
+    columna = int(input("Ingrese la columna : "))
+
+    
+
+
+
+
+
+
