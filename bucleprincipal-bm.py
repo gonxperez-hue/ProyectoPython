3652c5e5bdeaa427d58268a355bdfd3670771907
PabloAsisMuñoz <pabloasis02@gmail.com>
2026-02-13 11:24:58 +0100

    modificaciòn bucle principal "bm" + endgame

---
+++ bucleprincipal-bm.py
@@ -0,0 +1,14 @@
+#Si el juego ha terminado, mostrar el tablero con las minas descubiertas
+if juego_en_curso:
+    print("Ganaste, Enhorabuena!")
+    for i in range(filas):
+    for j in range(columnas):
+        if minas[i][j]:
+            print ("*", end = " ")
+        else:
+            print (visible[i][j], end = " ")
+    print()
+
+
+           
+            
--- endgame-bm.py
+++ endgame-bm.py
@@ -1,14 +1,19 @@
-#Si el juego ha terminado, mostrar el tablero con las minas descubiertas
-if juego_en_curso:
-    print("Ganaste, Enhorabuena!")
-    for i in range(filas):
-        for j in range(columnas):
-            if minas[i][j]:
-                print ("*", end = " ")
-            else:
-                print (visible[i][j], end = " ")
-           
-            #ESTE PRINT ESTA MAL UBICADO, AHORA MISMO ESTA HACIENDO UN SALTO DE LINEA POR CADA CELDA
-            print()    
+#En esta parte solicitaremos la acción del jugador
+opcion = input("Ingrese una de las tres opciones posible D para destapar, M para marcar U para desmarcar    : ")
 
-            
+#Procesamos la acción del jugador
+if opcion == "D":
+    #Solicitamos la fila y la columna a destapar
+    fila = int(input("Ingrese la fila : "))
+    columna = int(input("Ingrese la columna : "))
+
+    
+ 
+
+
+
+
+
+
+
+
