91460543cb9680d8e45ce2ce9770ecdc1ab46b1b
PabloAsisMuñoz <pabloasis02@gmail.com>
2026-02-13 11:34:52 +0100

    funcion "marcar" y "desmarcar" fila y columna

--- endgame-bm.py
+++ endgame-bm.py
@@ -6,13 +6,33 @@ if opcion == "D":
     #Solicitamos la fila y la columna a destapar
     fila = int(input("Ingrese la fila : "))
     columna = int(input("Ingrese la columna : "))
+
 #Destapar la casilla
 visible = [fila][columna] = True
+
 #Si la casilla contiene una mina, el jugador pierde
 if minas [fila][columna]:
     juego_en_curso = False
     print("Game over pare, a llorar a otro lado")
-    
+
+elif opcion == "M":
+    #Solicitar la fila y columna a marcar
+    fila = int(input("Ingrese la fila : "))
+    columna = int(input("Ingrese la columna : "))
+
+    #Marcar la casilla
+    visible [fila][columna] = True
+    minas [fila][columna] = True
+
+elif opcion == "U":
+    #Solicitar la fila y columna a desmarcar
+    fila = int(input("Ingrese la fila : "))
+    columna = int(input("Ingrese la columna : "))
+
+    #Desmarcar la casilla
+    visible [fila][columna] = False
+    minas [fila][columna] = False
+
 
 
     
