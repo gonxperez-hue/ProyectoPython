530d3902acf59245329804a7f0137397ba89a3b6
PabloAsisMuñoz <pabloasis02@gmail.com>
2026-02-13 11:28:13 +0100

    destapar casilla + game over si hay mina

--- endgame-bm.py
+++ endgame-bm.py
@@ -6,6 +6,14 @@ if opcion == "D":
     #Solicitamos la fila y la columna a destapar
     fila = int(input("Ingrese la fila : "))
     columna = int(input("Ingrese la columna : "))
+#Destapar la casilla
+visible = [fila][columna] = True
+#Si la casilla contiene una mina, el jugador pierde
+if minas [fila][columna]:
+    juego_en_curso = False
+    print("Game over pare, a llorar a otro lado")
+    
+
 
     
  
