2d2cf13311064dc8989d8974be0cc7d38803ec22
PabloAsisMuñoz <pabloasis02@gmail.com>
2026-02-13 11:36:56 +0100

    Fin del juego

--- endgame-bm.py
+++ endgame-bm.py
@@ -33,6 +33,18 @@ elif opcion == "U":
     visible [fila][columna] = False
     minas [fila][columna] = False
 
+#Si el juego ha terminado, mostrar el tablero con las minas descubiertas
+if juego_en_curso:
+    print("Ganaste, Enhorabuena campeón, de aqui a la once!")
+    for i in range(filas):
+        for j in range(columnas):
+            if minas[i][j]:
+                print ("*", end = " ")
+            else:
+                print (visible[i][j], end = " ")
+        print()
+
+
 
 
     
